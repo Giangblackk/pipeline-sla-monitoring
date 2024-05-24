d1727bcb1125904983f0270b1dc968768e19a943
giangblackk <giangblackk@gmail.com>
2024-05-24 18:39:26 +0700

    initial commit

---
+++ sample-pipeline-sla/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sample_pipeline_sla",
+    packages=find_packages(exclude=["sample_pipeline_sla_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
