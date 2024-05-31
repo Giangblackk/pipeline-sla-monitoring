5a22ac7e45deb753aad364cc7f4eb396126f20e3
giangblackk <giangblackk@gmail.com>
2024-05-31 08:40:21 +0700

    add sensors

--- sample-pipeline-sla/sample_pipeline_sla/__init__.py
+++ sample-pipeline-sla/sample_pipeline_sla/__init__.py
@@ -1,9 +1,10 @@
 from dagster import Definitions, load_assets_from_modules
 
-from . import assets
+from . import assets, sensors
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
+    sensors=[sensors.report_status_started]
 )
