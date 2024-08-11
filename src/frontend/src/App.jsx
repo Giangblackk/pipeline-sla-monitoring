e9b37de385f8682a0320353c01eaad6697ea774b
giangblackk <giangblackk@gmail.com>
2024-08-11 17:04:48 +0700

    switch from react ts to react js to fix build

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import KedroViz from '@quantumblack/kedro-viz'
 import '@quantumblack/kedro-viz/lib/styles/styles.min.css'
 import data from './main.json'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div style={{ height: `100%`, width: `100%`}}>
+    <div style={{ height: `100%`, width: `100%` }}>
       <KedroViz
         data={data}
       />
